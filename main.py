2066ed84a9fd0aa41711976773d35797a1022cac
rustwriter <156517192+rustwriter@users.noreply.github.com>
2024-01-29 12:15:35 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+def remove_duplicates():
+    input_file = "list.txt"
+    output_file = "list_wo_dupes.txt"
+
+    unique_lines = set()
+
+    with open(input_file, 'r') as file:
+        for line in file:
+            if line not in unique_lines:
+                unique_lines.add(line)
+            else:
+                print("Duplicate: " + line)
+
+    with open(output_file, 'w') as file:
+        for line in unique_lines:
+            file.write(line + '\n')
+
+if __name__ == '__main__':
+    remove_duplicates()
